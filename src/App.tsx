61018b53376f977ad3e313d2bfd0bb6d9f4d171a
oleksii-push <o.pushkash@ukma.edu.ua>
2025-11-02 16:54:51 +0200

    refactor: use one signal for nivels

--- src/App.tsx
+++ src/App.tsx
@@ -2,53 +2,12 @@ import { createSignal, For } from "solid-js";
 import { Stack, TextField, Typography } from "@suid/material";
 import { questions, Question } from "./questions";
 import { Math } from "./Math";
+import { Nivel } from "./Nivel";
 
 import "katex/dist/katex.min.css";
 
-function nivelMod(variant: number, divisor: number): number {
-    return variant % divisor;
-}
-
-function questionMod(variant: number, divisor: number): number {
-    return variant % divisor || variant;
-}
-
-interface NivelChoiceMod {
-    add: number;
-    divideBy: number;
-}
-
-class Nivel {
-    choiceMods: NivelChoiceMod[];
-    questions: Question[];
-
-    constructor(choiceMods: NivelChoiceMod[], questions: Question[]) {
-        this.choiceMods = choiceMods;
-        this.questions = questions;
-    }
-
-    chooseQuestionsForVariant(variant: number) {
-        return this.choiceMods.map(
-            (choiceMod) => nivelMod(variant + choiceMod.add, choiceMod.divideBy)
-        ).map((i) => {
-            let question = this.questions.at(i - 1);
-            let text = question.question;
-            const mods = question.mods;
-
-            for (const [variable, divisor] of Object.entries(mods)) {
-                text = text.replace("${" + variable + "}", questionMod(variant, divisor).toString());
-            }
-
-            return Object.assign(question, { question: text });
-        });
-    }
-}
-
 export default function App() {
-    const [nivelUnoQuestions, setNivelUnoQuestions] = createSignal<Question[]>();
-    const [nivelDosQuestions, setNivelDosQuestions] = createSignal<Question[]>();
-    const [nivelTresQuestions, setNivelTresQuestions] = createSignal<Question[]>();
-    const [nivelCuatroQuestions, setNivelCuatroQuestions] = createSignal<Question[]>();
+    const [nivels, setNivels] = createSignal<Record<number, Question[]>>({});
 
     const nivelUno = new Nivel([
         { add: 0, divideBy: 23 },
@@ -82,41 +41,27 @@ export default function App() {
                     style={"width: 25ch"}
                     defaultValue={0}
                     onChange={(_, value) => {
-                        setNivelUnoQuestions(nivelUno.chooseQuestionsForVariant(+value));
-                        setNivelDosQuestions(nivelDos.chooseQuestionsForVariant(+value));
-                        setNivelTresQuestions(nivelTres.chooseQuestionsForVariant(+value));
-                        setNivelCuatroQuestions(nivelCuatro.chooseQuestionsForVariant(+value));
+                        setNivels({
+                            1: nivelUno.chooseQuestionsForVariant(+value),
+                            2: nivelDos.chooseQuestionsForVariant(+value),
+                            3: nivelTres.chooseQuestionsForVariant(+value),
+                            4: nivelCuatro.chooseQuestionsForVariant(+value),
+                        });
                     }}
                 />
             </Stack>
             <Stack spacing={2}>
-                <Typography variant="h3">Nivel 1</Typography>
-                <ol>
-                    <For each={nivelUnoQuestions()}>
-                        {(item) => <li value={item.number}><Math text={item.question} /></li>}
-                    </For>
-                </ol>
-
-                <Typography variant="h3">Nivel 2</Typography>
-                <ol>
-                    <For each={nivelDosQuestions()}>
-                        {(item) => <li value={item.number}><Math text={item.question} /></li>}
-                    </For>
-                </ol>
-
-                <Typography variant="h3">Nivel 3</Typography>
-                <ol>
-                    <For each={nivelTresQuestions()}>
-                        {(item) => <li value={item.number}><Math text={item.question} /></li>}
-                    </For>
-                </ol>
-
-                <Typography variant="h3">Nivel 4</Typography>
-                <ol>
-                    <For each={nivelCuatroQuestions()}>
-                        {(item) => <li value={item.number}><Math text={item.question} /></li>}
-                    </For>
-                </ol>
+                <For each={Object.entries(nivels())}>
+                    {([nivel, questions]) =>
+                        <div>
+                            <Typography variant="h3">Nivel {nivel}</Typography>
+                            <ol>
+                                <For each={questions}>
+                                    {(item) => <li value={item.number}><Math text={item.question} /></li>}
+                                </For>
+                            </ol>
+                        </div>}
+                </For>
             </Stack>
         </Stack>
     );
