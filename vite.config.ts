855c13c5f73ab9b6130811df11e7aa44f7a8296d
oleksii-push <o.pushkash@ukma.edu.ua>
2026-01-18 11:14:29 +0200

    fix: base url for github pages project site

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import suidPlugin from "@suid/vite-plugin";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
+  base: "/shchehloviium/",
   plugins: [suidPlugin(), solidPlugin()],
   build: {
     target: "esnext",
